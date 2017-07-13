e172a41d440bef78a3b75536b78f0ef84b00f752
Akshay <kumarsharmaa69@gmail.com>
2017-07-13 18:57:50 +0530

    Added floating button and added menu options (about, settings)

--- app/src/main/java/com/pnote/tw/pocketnote/Dashboard.java
+++ app/src/main/java/com/pnote/tw/pocketnote/Dashboard.java
@@ -3,12 +3,17 @@ package com.pnote.tw.pocketnote;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.pnote.tw.pocketnote.about.About;
 import com.pnote.tw.pocketnote.utils.NotesListener;
 import com.pnote.tw.pocketnote.utils.NotesManager;
+import com.pnote.tw.pocketnote.utils.Settings;
 
 public class Dashboard extends AppCompatActivity {
 
@@ -27,6 +32,26 @@ public class Dashboard extends AppCompatActivity {
 
         NotesListener notesListener = new NotesListener(this);
         notes.setOnItemClickListener(notesListener);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.navigation, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.navigation_setting:
+                startActivity(new Intent(this, Settings.class));
+                return true;
+            case R.id.navigation_about:
+                startActivity(new Intent(this, About.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     public void addNewNote(View view) {
